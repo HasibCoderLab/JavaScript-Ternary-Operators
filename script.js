c92846e0c3daff3422cd595723a478ed422be8e5
Hasib Hasan <mohammodhasibhasan@gmail.com>
2025-02-24 15:21:36 +0600

    2n Problem Solve

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ console.log(checkPositive(10))
 // Output : Positive
 console.log(checkPositive(-10))
  // Output : Negative
+
+const checkAge = (age) => age >= 18 ? "Adult" : "Minor"
+console.log(checkAge(14))
+ // Output : Minor
+console.log(checkAge(20))
+ // Output : Adult
