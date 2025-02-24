081582dbd0608ad23d203eca54b70d168b0f700b
Hasib Hasan <mohammodhasibhasan@gmail.com>
2025-02-24 09:27:57 +0600

    Solve 1st Problem

---
+++ script.js
@@ -0,0 +1,6 @@
+// ==  Solve Problem With JavaScript Ternary Operator ==== 
+const checkPositive = (input) => input > 0 ? "Positive" :input < 0 ? "Negative" :"Zero"
+console.log(checkPositive(10))
+// Output : Positive
+console.log(checkPositive(-10))
+ // Output : Negative
